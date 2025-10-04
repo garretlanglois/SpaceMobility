49950a23bb98bdf1a9cdf12d3d2adf944a0fecd5
Garret Langlois <gaslanglois@gmail.com>
2025-10-04 14:45:57 -0400

    Initial splash screen implementation

--- app/designer/page.tsx
+++ app/designer/page.tsx
@@ -1,8 +1,15 @@
+"use client";
+
+import { useState } from "react";
 import SceneDesigner from "@/components/SceneDesigner";
+import SplashScreen from "@/components/SplashScreen";
 
 export default function DesignerPage() {
+  const [showSplash, setShowSplash] = useState(true);
+
   return (
     <main style={{ height: "100vh", width: "100vw", overflow: "hidden" }}>
+      {showSplash && <SplashScreen onComplete={() => setShowSplash(false)} />}
       <SceneDesigner />
     </main>
   );
